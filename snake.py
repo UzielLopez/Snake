0b746217452a680c6d97182cb3fea05890084a90
echazaro <echazaro@gmail.com>
2021-05-06 19:40:39 -0500

    movimiento aleatorio comida

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 from turtle import *
 from random import randrange
+from random import randint
 from freegames import square, vector
 
 food = vector(0, 0)
@@ -11,14 +12,37 @@ def change(x, y):
     aim.x = x
     aim.y = y
 
+
+
 def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+def insideFood(foodHead):
+    "Return True if head inside boundaries."
+    return -200 < foodHead.x < 190 and -200 < foodHead.y < 190
+
 def move():
     "Move snake forward one segment."
+    foodHead = food.copy()
     head = snake[-1].copy()
     head.move(aim)
+
+    #for pos in range(100):
+    #food.x = (food.x)+(randrange(-1,1)*randrange(-1, 1)*10)
+    #food.y = (food.y)+(randrange(-1,1)*randrange(-1, 1)*10)
+    luck=randint(0,3)
+    print(luck)
+    
+    if luck == 0:
+        food.x = (food.x)+(randint(-1, 1)*10)
+    
+    if luck == 1:
+        food.y = (food.y)+(randint(-1,1)*randint(-1, 1)*10)
+
+    if not insideFood(foodHead) :
+        food.x = 0
+        food.y = 0
 
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
@@ -33,7 +57,8 @@ def move():
         food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
-
+        
+        
     clear()
 
     for body in snake:
